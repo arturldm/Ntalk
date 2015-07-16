9a8c04f262af8fc88a65f0c26ce1aadd87b82507
arturldm <artur_ldm@hotmail.com>
2015-07-16 15:39:11 -0300

    Adição de contatos

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var express = require('express'),
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 
